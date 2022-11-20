681c8b9ef1fbdcbffa733408f24cd7e508461d9c
carrolja <47407800+carrolja@users.noreply.github.com>
2022-11-20 14:05:40 -0500

    Deleted C directory (unused)

--- C/freq.h
+++
@@ -1,6 +0,0 @@
-const int A1_freq = 109;
-const int A1_freq = 109;
-const int A2_freq = 219;
-const int A3_freq = 439;
-const int A4_freq = 879;
-const int A5_freq = 1759;
--- C/main.c
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "freq.h"
-
-int main(int argc, char ** argv) {
-	return 0;
-}
